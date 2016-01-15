ac0b9e3693cc745f5c6626b6a5bf4c591b1a2d1c
Yigal Weinstein <yigal@8leggedunicorn.com>
2016-01-15 07:36:51 -0600

    Added file filter for download as a cli option

--- gh_subdir_downl.py
+++ gh_subdir_downl.py
@@ -2,8 +2,9 @@
 
 '''
 description:
-    This script was written to download subdirectories of GitHub repositories.
-    It was created as GitHub doesn't provide archiving, i.e. git-archive, see
+    This script was written to download an archive, i.e. no history or
+    meta-information of a GitHub repository or subdirectory of a repo..  It was
+    created as GitHub doesn't provide archiving, i.e. git-archive, see
     https://help.github.com/articles/can-i-archive-a-repository/
 
     There is an internal variable specifying via regex the file filter to use
@@ -24,46 +25,66 @@ import argparse
 __author__ = 'Yigal Weinstein'
 
 parser = argparse.ArgumentParser()
-parser.add_argument("-v", "--verbose", help="increase output verbosity", action="store_true")
-parser.add_argument("-u", "--user", type=str, help="specify the GitHub user account", required=True)
-parser.add_argument("-r", "--repository", type=str, help="specify the GitHub user's repository", required=True)
-parser.add_argument("-s", "--subdirectory", type=str, help="specify the subdirectory to download", required=True)
+parser.add_argument("-v", "--verbose", help="increase output verbosity", 
+        action="store_true")
+parser.add_argument("-u", "--user", type=str, help="specify the GitHub user account", 
+        required=True)
+parser.add_argument("-r", "--repository", type=str, help="specify the GitHub user's repository", 
+        required=True)
+parser.add_argument("-s", "--subdirectory", type=str, help="specify a subdirectory to download", 
+        required=False)
+fhelp = '''
+Specify via regex what files to download.  As an example: 
+
+-f 'README|.*\.(csv|bib|py|tex)'
+
+to download only csv, bib, py and tex files.
+'''
+parser.add_argument("-f", "--filetypes", type=str, help = fhelp, 
+        required=False)
 
 args = parser.parse_args()
 
 user = args.user
 repo = args.repository
-subdir = args.subdirectory
-path = subdir
 
-#filetypes = 'README|.*\.(csv|bib|py|tex)'
-filetypes = '.*'
+if args.subdirectory:
+    subdir = args.subdirectory
+    path = subdir
+else:
+    path = ''
+
+if args.filetypes:
+    filetypes = args.filetypes
+    #filetypes = 'README|.*\.(csv|bib|py|tex)'
+else:
+    filetypes = '.*'
 
 if args.verbose:
     print("verbosity turned on")
 
-def gen_url( user, repo, path ):
+def gen_url( user, repo, path='' ):
     url = 'https://api.github.com/repos/' + user + '/' + repo + \
         '/contents/' + path
     return url
 
 def ls_subdir_content( url, path ):
-    # creates subdir and returns list of objects - files, and directories
-    if not os.path.exists(path):
-        os.makedirs(path)
+    # returns list of objects - files, and directories contained within a
+    # repository or subdirectory 
 
     headers = { 'Accept' : 'application/vnd.github.v3.raw' }
     r = requests.get( url, headers = headers )
     contents = json.loads( r.text )
     return contents
 
-
 def get_file( url, file_name, path ):
+    if not os.path.exists(path) and path != '':
+        os.makedirs(path)
     # Function to download a file
     file_path = os.path.join( path, file_name )
     response = requests.get( url, stream=True )
 
-    print( 'Direct link {url}\nDownloading {s}'.format( url = url, s = file_name ) )
+    print( '\nDirect link {url}\nDownloading {s}'.format( url = url, s = file_name ) )
 
     file_content = requests.get(url).text #.encode('utf-8')
     with open( file_path , 'w' ) as out_file:
@@ -74,12 +95,12 @@ def recurs_dl( contents , path ):
     for obj in contents:
         if obj[ 'type' ] == 'file':
             file_name = obj[ 'name' ]
-            if re.search(filetypes, file_name): # only download selected files
+            # filter downloads using the regex obj 'filetypes'
+            if re.search(filetypes, file_name):
                 url = obj[ 'download_url' ]
                 get_file( url, file_name, path )
         elif obj[ 'type' ] == 'dir':
             subdir_path = os.path.join( path , obj[ 'name' ] )
-            print(subdir_path)
             subdir_url = gen_url( user, repo, subdir_path )
             subdir_contents = ls_subdir_content( subdir_url, subdir_path )
             recurs_dl( subdir_contents , subdir_path )
